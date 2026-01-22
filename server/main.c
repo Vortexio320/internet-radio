2ff207df947bd0d5f62ac861d5bcac49e5f9c9bd
Michal Byczko <m.byczko@wp.pl>
2026-01-22 22:36:40 +0000

    Server: Added multithreading (pthread) handling multiple clients

--- server/main.c
+++ server/main.c
@@ -3,18 +3,35 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define PORT 8080
-#define BUFFER_SIZE 1024
+
+void *handle_client(void *socket_desc) {
+    int sock = *(int*)socket_desc;
+    
+    free(socket_desc);
+
+    char *message = "Witaj! Jestes podlaczony do serwera wielowatkowego.\n";
+    send(sock, message, strlen(message), 0);
+    
+    printf("Watek: Obsluzylem klienta na sockecie %d.\n", sock);
+
+    //10 sekund sluchania radia, potem bedzie rozwiniete
+    sleep(10); 
+
+    printf("Watek: Klient rozlaczony.\n");
+    close(sock);
+    return NULL;
+}
 
 int main() {
-    int server_fd, new_socket;
+    int server_fd, *new_sock;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char *hello = "Witaj! Tu Twoj serwer radiowy.\n";
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Blad tworzenia gniazda");
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
@@ -26,31 +43,37 @@ int main() {
     address.sin_port = htons(PORT);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Blad bindowania");
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, 3) < 0) {
-        perror("Blad listen");
+        perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Serwer nasluchuje na porcie %d...\n", PORT);
+    printf("Serwer wielowatkowy gotowy na porcie %d...\n", PORT);
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("Blad accept");
-        exit(EXIT_FAILURE);
+    while (1) {
+        new_sock = malloc(sizeof(int));
+        
+        if ((*new_sock = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("Accept failed");
+            free(new_sock);
+            continue;
+        }
+
+        printf("Main: Nowe polaczenie! Tworze watek...\n");
+
+        pthread_t thread_id;
+        if (pthread_create(&thread_id, NULL, handle_client, (void*)new_sock) < 0) {
+            perror("Nie udalo sie utworzyc watku");
+            free(new_sock);
+            continue;
+        }
+
+        pthread_detach(thread_id);
     }
-
-    printf("Nowe polaczenie nawiazane!\n");
-
-    // 6. Wyslanie powitania
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Powitanie wyslane.\n");
-
-    // 7. Zamkniecie polaczenia
-    close(new_socket);
-    close(server_fd);
 
     return 0;
 }
